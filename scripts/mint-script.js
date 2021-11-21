a930e8a1fd5d54f400c81e9cc0109e07356e9098
Juan Castellano <juancastellano115@gmail.com>
2021-11-21 13:48:34 +0100

    IPFS changes

--- scripts/mint-script.js
+++ scripts/mint-script.js
@@ -1,10 +1,10 @@
 const hre = require("hardhat");
 async function main() {
   const NFT = await hre.ethers.getContractFactory("ZaHando");
-  const URI01 = "https://ipfs.io/ipfs/QmeGQuGPVeTMosC16vZEULggAebVXq44CKzdNTK71F3uNt"
+  const URI01 = "ipfs://QmdJ1Xmb1zxAyeDspuA7Dcz49Agfgskpueh5GM2G9DN9eX"
   const URI02 = "ipfs://QmSJhBappVGD54jyr92YgCiHRezsT9rDZxxQx9H7gyJ2Sb"
   const WALLET_ADDRESS = "0x7d15210440ECa5FCdD2CfC9e9375A238CAd14dF4"
-  const CONTRACT_ADDRESS = "0x2544CF12044e5E984997525dCeB858F5aB45C82f"
+  const CONTRACT_ADDRESS = "0x86F5EF671b132FB5587492a097EdA61A2B17a2E8"
   const contract = NFT.attach(CONTRACT_ADDRESS);
   await contract.mint(WALLET_ADDRESS, URI01);
   console.log("NFT minted: 01");
